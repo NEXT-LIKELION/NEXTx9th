2b94ec9c17210e31168c5f5eefb35414b6e8a611
OrcaLion <OrcaLion@likelion.org>
2021-05-30 10:32:48 +0900

    feat : new base for hackathon

--- Session5/CountProject/CountApp/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CountappConfig(AppConfig):
-    name = 'CountApp'
--- Session6/test.py
+++
@@ -1,6 +0,0 @@
-class Test():
-    def __str__(self):
-        return "hello"
-
-test_instance = Test()
-print(test_instance)
